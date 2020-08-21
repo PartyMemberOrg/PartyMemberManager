63068ae0c5e69d4d44b5de69ab9373ca6983e4c1
zhxud <zhxud@DESKTOP-RNJ90PS>
2020-08-21 08:41:37 +0800

    增加模板下载

---
+++ PartyMemberManager/Controllers/DownloadController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PartyMemberManager.Controllers
+{
+    public class DownloadController : Controller
+    {
+        public IActionResult ExcelTemplates(string fileName)
+        {
+            return File(System.IO.Path.Combine(AppContext.BaseDirectory, "ExcelTemplates", fileName), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        }
+    }
+}
