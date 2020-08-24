0a6498781b24d81e4d35c3488ba07284a81f71cf
343931259@qq.com <343931259@qq.com>
2020-08-24 13:16:11 +0800

    ypf

---
+++ PartyMemberManager/Models/ActivistTrainResultImportViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyMemberManager.Models
+{
+    /// <summary>
+    /// 入党积极分子成绩数据导入视图模型
+    /// </summary>
+    public class ActivistTrainResultImportViewModel: PartyActivistImportViewModel
+    {
+    }
+}
