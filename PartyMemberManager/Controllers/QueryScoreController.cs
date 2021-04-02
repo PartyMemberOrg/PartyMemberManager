e185201c35e64d6d590ff462d81883aa9dd711b6
Yangpengfei <343931259@qq.com>
2021-04-02 18:04:08 +0800

    添加查询

---
+++ PartyMemberManager/Controllers/QueryScoreController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PartyMemberManager.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PartyMemberManager.Framework.Controllers;
+using Microsoft.AspNetCore.Http;
+using PartyMemberManager.Dal;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
+using PartyMemberManager.Core.Exceptions;
+using Microsoft.EntityFrameworkCore;
+using PartyMemberManager.Dal.Entities;
+
+namespace PartyMemberManager.Controllers
+{
+
+    public class QueryScoreController : PartyMemberControllerBase
+    {
+        public QueryScoreController(ILogger<QueryScoreController> logger, PMContext context, IHttpContextAccessor accessor) : base(logger, context, accessor)
+        {
+        }
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public IActionResult GetQuery()
+        {
+            ViewBag.TrainClassTypeId = new SelectList(_context.TrainClassTypes.Where(d => d.Code.StartsWith("4")).OrderByDescending(d => d.Code), "Id", "Name");
+            return View("query");
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult PostQuery(QueryScoreViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new PartyMemberException("查询数据不合法");
+            }
+            var trainClassType = _context.TrainClassTypes.Find(model.TrainClassTypeId);
+            if (trainClassType.Name.Contains("入党积极分子"))
+            {
+                ActivistTrainResult activistTrainResult = _context.ActivistTrainResults.Include(d => d.PartyActivist)
+                    .Where(d => d.PartyActivist.Name == model.Name && d.PartyActivist.IdNumber == model.IdNumber && d.PartyActivist.JobNo == model.JobNo).FirstOrDefault();
+                if (activistTrainResult != null && activistTrainResult.PartyActivist.IsPrint)
+                {
+                    model.TrainClassType = trainClassType;
+                    model.IsPrint = activistTrainResult.PartyActivist.IsPrint;
+                    model.TotalGrade = activistTrainResult.TotalGrade;
+                    return View(model);
+                }
+                else if(activistTrainResult != null && !activistTrainResult.PartyActivist.IsPrint)
+                {
+                    model.IsPrint = false;
+                    model.Message = "你的成绩还未录入";
+                    return View(model);
+                }
+            }
+            else if (trainClassType.Name.Contains("发展对象"))
+            {
+                PotentialTrainResult potentialTrainResult = _context.PotentialTrainResults.Include(d => d.PotentialMember)
+                   .Where(d => d.PotentialMember.Name == model.Name && d.PotentialMember.IdNumber == model.IdNumber && d.PotentialMember.JobNo == model.JobNo).FirstOrDefault();
+                if (potentialTrainResult != null && potentialTrainResult.PotentialMember.IsPrint)
+                {
+                    model.TrainClassType = trainClassType;
+                    model.IsPrint = potentialTrainResult.PotentialMember.IsPrint;
+                    model.TotalGrade = potentialTrainResult.TotalGrade;
+                    return View(model);
+                }
+                else if (potentialTrainResult != null && !potentialTrainResult.PotentialMember.IsPrint)
+                {
+                    model.IsPrint = false;
+                    model.Message = "你的成绩还未录入";
+                    return View(model);
+                }
+            }
+            model.IsPrint = false;
+            model.Message = "未查询到改学员的信息，请核对";
+            return View(model);
+        }
+    }
+}
---
+++ PartyMemberManager/Models/QueryScoreViewModel.cs
@@ -0,0 +1,63 @@
+﻿using PartyMemberManager.Dal.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyMemberManager.Models
+{
+    public class QueryScoreViewModel
+    {
+
+        /// <summary>
+        /// 培训班类型
+        /// </summary>
+        [DisplayName("培训班类型")]
+        public Guid TrainClassTypeId { get; set; }
+
+        /// <summary>
+        /// 培训班类型
+        /// </summary>
+        [DisplayName("培训班类型")]
+        public TrainClassType TrainClassType { get; set; }
+
+        /// <summary>
+        /// 培训班类型
+        /// </summary>
+        [DisplayName("培训班类型")]
+        public string TrainClassTypeDisplay { get => TrainClassType == null ? "" : TrainClassType.Name; }
+
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        [DisplayName("姓名")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 学号/工号
+        /// </summary>
+        [DisplayName("学号/工号")]
+        public string JobNo { get; set; }
+
+
+        /// <summary>
+        /// 身份证号
+        /// </summary>
+        [DisplayName("身份证号")]
+        public string IdNumber { get; set; }
+        /// <summary>
+        /// 总评成绩
+        /// </summary>
+        [DisplayName("总成绩")]
+        public decimal? TotalGrade { get; set; }
+
+        /// <summary>
+        /// 是否已打印
+        /// </summary>
+        [DisplayName("是否已打印")]
+        public bool IsPrint { get; set; }
+
+        public string Message { get; set; }
+    }
+}
